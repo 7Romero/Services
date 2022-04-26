52401245a551655a3327662d578075fd4276d78c
7Romero <romero.serhio1@gmail.com>
2022-04-26 13:39:58 +0300

    Added configuration for JWT auth

--- src/Services.API/Program.cs
+++ src/Services.API/Program.cs
@@ -1,11 +1,68 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Services.API.Infrastructure.Extensions;
+using Services.Bll;
+using Services.Dal;
+using Services.Dal.Interfaces;
+using Services.Dal.Repositories;
+using Services.Domain.Auth;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+
+// For Entity Framework
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Adding Authentication
+
+builder.Services.AddIdentity<User, Role>(options =>
+{
+    options.Password.RequiredLength = 5;
+})
+.AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IGenericRepository, GenericRepository>();
+var authOptions = builder.Services.ConfigureAuthOptions(configuration);
+builder.Services.AddJwtAuthentication(authOptions);
+//builder.Services.AddScoped<IUserService, UserService>();
+
+/*builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("JWT:Secret").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });*/
+
+builder.Services.AddAutoMapper(typeof(BllAssemblyMarker));
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 
 var app = builder.Build();
 
@@ -18,6 +75,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Authentication & Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
