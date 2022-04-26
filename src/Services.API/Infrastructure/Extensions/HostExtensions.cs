f5d2f76edcc2e7cee8f2ba1a94235466acab0780
7Romero <romero.serhio1@gmail.com>
2022-04-27 00:34:19 +0300

    Created extensions for host

---
+++ src/Services.API/Infrastructure/Extensions/HostExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Services.Dal;
+using Services.Dal.Seed;
+using Services.Domain.Auth;
+
+namespace Services.API.Infrastructure.Extensions
+{
+    public static class HostExtensions
+    {
+        public static async Task SeedData(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    context.Database.Migrate();
+
+                    await UsersSeed.Seed(userManager);
+                    await SkillSeed.Seed(context);
+                    await SectionsSeed.Seed(context);
+                    await CategoriesSeed.Seed(context);
+                    await OrdersSeed.Seed(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+        }
+    }
+}
