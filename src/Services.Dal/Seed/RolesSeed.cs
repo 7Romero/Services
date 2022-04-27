01e7a7b4d7ff7314b2fb1394c4eaa3fe407a3a35
7Romero <romero.serhio1@gmail.com>
2022-04-27 11:49:25 +0300

    Created seed for roles

---
+++ src/Services.Dal/Seed/RolesSeed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Services.Domain.Auth;
+
+namespace Services.Dal.Seed
+{
+    public class RolesSeed
+    {
+        public static async Task Seed(RoleManager<Role> roleManager)
+        {
+            if (!roleManager.Roles.Any())
+            {
+                var roles = new List<Role>()
+                {
+                    new Role { Name = "User" },
+                    new Role { Name = "Moderator" },
+                    new Role { Name = "Administrator" },
+                    new Role { Name = "Owner" },
+                };
+
+                foreach (var role in roles)
+                {
+                    await roleManager.CreateAsync(role);
+                }
+
+            }
+        }
+    }
+}
